008c9a8551be9c8b6608493c5ac1ffcd68e86c73
rice8y <eitoy1128@mbr.nifty.com>
2025-08-05 17:25:24 +0900

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mkdir -p tmp-ybbb && cd tmp-ybbb
 git clone https://github.com/rice8y/ybbb.git
-cd ybbb
-uv tool install -e ./bench
-cd ../../ && rm -rf tmp-ybbb
+cd ybbb/bench
+uv tool install -e .
+cd ../../ && rm -rf ybbb
